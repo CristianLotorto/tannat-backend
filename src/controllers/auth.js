9481d69f2ac463ce93ef293bf7bbf9e90da39b2b
YhanahiRosenthal <yhanahi16@gmail.com>
2024-01-10 14:40:18 -0300

    auth modified, add role to data and a one more validation to username

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -12,13 +12,25 @@ const getAuth = async (req, res) => {
 
     if (admin) {
         const validate = await bcrypt.compare(password, admin.password)
-        if (validate){
-            const accessToken = jwt.sign({username,password}, process.env.TOKEN_SECRET, {expiresIn: '1800s'})
-            return res.status(201).json({
-                message: 'User found',
-                data: accessToken,
-                error: false,
-              });
+
+        if(admin.username === username) {
+          if (validate){
+              const accessToken = jwt.sign({username,password}, process.env.TOKEN_SECRET, {expiresIn: '1800s'})
+              return res.status(200).json({
+                  message: 'User found',
+                  data: {
+                    token: accessToken,
+                    role: admin.role
+                  },
+                  error: false,
+                });
+          }
+        } else {
+          return res.status(404).json({
+            message: 'Invalid credentials',
+            data: null,
+            error: true,
+          });
         }
     }
 
